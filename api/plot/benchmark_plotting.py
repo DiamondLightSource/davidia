6870a31e34d3098c5cd8555ce4354a0f6731ac29
VictoriaBeilsten-Edmands <victoria.beilsten.edmands@gmail.com>
2022-09-26 22:13:18 +0100

    Add benchmark plotting script

--- api/plot/benchmark_plotting.py
+++ api/plot/benchmark_plotting.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import argparse
 import datetime
-import json
+import msgpack
 import requests
 import sys
 import time
@@ -12,21 +12,26 @@ from custom_types import PlotMessage
 
 def plot_points(args) -> requests.Response:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--points", type=int, default= 10, required=False)
+    parser.add_argument("-p", "--points", type=int, default=10, required=False)
     args = parser.parse_args(args)
 
     x = [i for i in range(args.points)]
     y = [j % 10  for j in x]
     time_id = datetime.datetime.now().strftime(f"%Y%m%d%H%M%S")
 
-    aux_line = PlotMessage(type="aux_line_data", params={"id": time_id, "colour": "purple", "x": x, "y": y})
-    aux_line_as_json = json.dumps(aux_line.__dict__)
+    aux_line = PlotMessage(type="aux_line_data", params={"plot_id":"0", "id": time_id, "colour": "purple", "x": x, "y": y})
+    msg = msgpack.packb(aux_line.__dict__, use_bin_type=True)
+
+    url = 'http://localhost:8000/push_data'
+    headers = {'content-type': 'application/x-msgpack', 'accept' : 'application/x-msgpack'}
 
     start_time = time.time()
-    response = requests.get('http://localhost:8000/push_data', params={'message': aux_line_as_json}, headers={'Content-type': 'application/json'}, auth=('user', 'pass'))
+    response =  requests.post(url, data=msg, headers=headers, auth=('user', 'pass'))
     end_time = time.time()
 
     print(f"{args.points} plotted in {end_time - start_time}s.\n")
+    print(f"response status code is {response.status_code}. \n")
+
     return response
 
 
