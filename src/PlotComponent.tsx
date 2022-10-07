ba155a45943d8203eb1415995b02deb302e48afb
VictoriaBeilsten-Edmands <victoria.beilsten.edmands@gmail.com>
2022-10-07 16:43:09 +0100

    Move Plot into PlotComponent

--- src/PlotComponent.tsx
+++ src/PlotComponent.tsx
@@ -1,8 +1,8 @@
 import '@h5web/lib/dist/styles.css';
-import { CurveType } from '@h5web/lib';
-import React from 'react';
+import { CurveType, DataCurve, HeatmapVis, ResetZoomButton, SelectToZoom, TooltipMesh, VisCanvas } from '@h5web/lib';
 import { decode } from "messagepack";
-import Plot from "./Plot"
+import ndarray from 'ndarray';
+import React from 'react';
 
 
 interface LinePlotParameters {
@@ -10,18 +10,66 @@ interface LinePlotParameters {
   xDomain: [number, number];
   yDomain: [number, number];
   curveType: CurveType;
+  }
+
+
+interface HeatPlotParameters {
+  values: ndarray.NdArray<number[]>,
+  domain: [number, number],
+  }
+
+
+function instanceOfHeatPlotParameters(object: any): object is HeatPlotParameters {
+    return 'values' in object;
 }
 
+
 type PlotProps = {
+    plotParameters: LinePlotParameters | HeatPlotParameters
+  };
+
+class Plot extends React.Component<PlotProps> {
+  render() {
+    if (instanceOfHeatPlotParameters(this.props.plotParameters)) {
+    return (
+      <>
+      <HeatmapVis colorMap="Warm" dataArray={this.props.plotParameters.values} domain={this.props.plotParameters.domain} layout="fill" scaleType="linear" showGrid>
+      </HeatmapVis>
+      </>
+      );
+    }
+    else {
+      let curveType = this.props.plotParameters.curveType;
+      return (
+        <>
+        <VisCanvas
+        abscissaConfig={{ visDomain: this.props.plotParameters.xDomain, showGrid: true }}
+        ordinateConfig={{ visDomain: this.props.plotParameters.yDomain, showGrid: true }}
+        >
+        {Array.from(this.props.plotParameters.data).map(d => <DataCurve key={d.id} abscissas={d.x} ordinates={d.y} color={d.colour} curveType={curveType}/>)}
+        <TooltipMesh renderTooltip={(x, y) => <p>{y}</p>} />
+        <SelectToZoom/>
+        <ResetZoomButton/>
+        </VisCanvas>
+    </>
+      );
+    }
+  }
+}
+
+
+type PlotComponentProps = {
   plot_id: string,
   hostname: string,
   port: string
 };
+
 type PlotStates = {
   multilineData: LineData[]};
-class PlotComponent extends React.Component<PlotProps, PlotStates> {
+
+class PlotComponent extends React.Component<PlotComponentProps, PlotStates> {
   public static defaultProps = {hostname: "ws://127.0.0.1", port: "8000"};
-  constructor(props: PlotProps) {
+  constructor(props: PlotComponentProps) {
     super(props)
     this.state = {multilineData: []}
   }
