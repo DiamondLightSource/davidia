92428cba5e70294b7ff83fb3802dc3eee27a6960
Peter Chang <peter.chang@diamond.ac.uk>
2022-10-12 22:45:31 +0100

    Remove web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-// import { createRoot } from 'react-dom/client';
-// const container = document.getElementById('App');
-// const root = createRoot(container!); 
-// root.render(<App />);
-
-const container = document.getElementById('root') as HTMLElement;
-render(
-    <App />, container);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<App />, document.getElementById('root'));
