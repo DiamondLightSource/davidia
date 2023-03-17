d2641c5f27db5246a42d661f03140bd8381d8604
Peter Chang <peter.chang@diamond.ac.uk>
2023-03-17 14:01:03 +0000

    Fix rectangle and line selections
    Also make ID unique per instance on server, add methods to selection
    classes to aid their creation, and add prop to change selection type

--- client/component/selections.tsx
+++ client/component/selections.tsx
@@ -9,6 +9,23 @@ import { Matrix3, Vector3 } from 'three';
 import DvdPolygon from './shapes/DvdPolygon';
 import DvdPolyline from './shapes/DvdPolyline';
 
+export enum SelectionType {
+  line,
+  rectangle,
+  polyline,
+  polygon,
+  circle,
+  ellipse,
+  sector,
+  unknown,
+}
+
+function polar(xy: Vector3): [number, number] {
+  const x = xy.x;
+  const y = xy.y;
+  return [Math.hypot(y, x), Math.atan2(y, x)];
+}
+
 /** export class for all selections */
 class BaseSelection implements SelectionBase {
   id: string;
@@ -25,7 +42,26 @@ class BaseSelection implements SelectionBase {
   }
 
   getPoints() {
-    return [new Vector3(...this.start)];
+    return [this.vStart.clone()];
+  }
+
+  setProperties(id: string, name: string, colour?: string, alpha?: number) {
+    this.id = id;
+    this.name = name;
+    this.colour = colour;
+    this.alpha = alpha ?? 1;
+  }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  setFixed(fixed: boolean) {
+    this.fixed = fixed;
+  }
+
+  static isShape(s: BaseSelection | SelectionBase): s is BaseSelection {
+    return 'vStart' in s;
   }
 }
 
@@ -36,12 +72,18 @@ export class OrientableSelection extends BaseSelection {
   constructor(start: [number, number], angle = 0) {
     super(start);
     this.angle = angle;
-    this.transform = new Matrix3().identity().rotate(this.angle);
+    this.transform = new Matrix3().identity().rotate(-this.angle);
   }
 
   setAngle(angle: number) {
     this.angle = angle;
-    this.transform = new Matrix3().identity().rotate(this.angle);
+    this.transform = new Matrix3().identity().rotate(-this.angle);
+  }
+
+  static isShape(
+    s: OrientableSelection | SelectionBase
+  ): s is OrientableSelection {
+    return 'transform' in s;
   }
 }
 
@@ -56,14 +98,30 @@ export class LinearSelection extends OrientableSelection {
   getPoints(): Vector3[] {
     const m = this.transform;
     const s = this.vStart.clone();
-    return [s, new Vector3(this.length, 0, 1).applyMatrix3(m).add(s)];
+    const e = new Vector3(this.length, 0, 1).applyMatrix3(m).add(s);
+    return [s, e];
+  }
+
+  static clicks() {
+    return [2, 2];
+  }
+
+  static isShape(s: LinearSelection | SelectionBase): s is LinearSelection {
+    return 'length' in s;
   }
 
   static createFromPoints(points: Vector3[]) {
     const b = points[0];
     const l = new Vector3().subVectors(points[1], b);
-    const a = Math.atan2(l.y, l.x);
-    return new LinearSelection([b.x, b.y], Math.hypot(l.x, l.y), a);
+    const pl = polar(l);
+    return new LinearSelection([b.x, b.y], pl[0], pl[1]);
+  }
+
+  static createFromSelection(s: LinearSelection) {
+    const l = new LinearSelection(s.start, s.length, s.angle);
+    l.setProperties(s.id, s.name, s.colour, s.alpha);
+    l.setFixed(s.fixed);
+    return l;
   }
 }
 
@@ -86,6 +144,16 @@ export class RectangularSelection extends OrientableSelection {
     const m = this.transform;
     const s = this.vStart;
     return all.map((a) => a.applyMatrix3(m).add(s));
+  }
+
+  static clicks() {
+    return [2, 2];
+  }
+
+  static isShape(
+    s: RectangularSelection | SelectionBase
+  ): s is RectangularSelection {
+    return 'lengths' in s;
   }
 
   static createFromPoints(data: boolean, points: Vector3[]) {
@@ -126,22 +194,52 @@ export class RectangularSelection extends OrientableSelection {
     }
     return new RectangularSelection([b.x, b.y], [l.x, l.y], a);
   }
+
+  static createFromSelection(s: RectangularSelection) {
+    const r = new RectangularSelection(s.start, s.lengths, s.angle);
+    r.setProperties(s.id, s.name, s.colour, s.alpha);
+    r.setFixed(s.fixed);
+    return r;
+  }
 }
 
 /** export class to select a polygon */
 export class PolygonalSelection extends BaseSelection {
   points: [number, number][];
-  constructor(points: [number, number][]) {
+  closed: boolean;
+  constructor(points: [number, number][], closed?: boolean) {
     super(points[0]);
     this.points = points;
+    this.closed = closed ?? true;
   }
 
   getPoints(): Vector3[] {
     return this.points.map((p) => new Vector3(...p));
   }
 
-  static createFromPoints(points: Vector3[]) {
-    return new PolygonalSelection(points.map((p) => [p.x, p.y]));
+  static clicks() {
+    return [2, -1];
+  }
+
+  static isShape(
+    s: PolygonalSelection | SelectionBase
+  ): s is PolygonalSelection {
+    return 'points' in s;
+  }
+
+  static createFromPoints(closed: boolean, points: Vector3[]) {
+    const p = new PolygonalSelection(
+      points.map((p) => [p.x, p.y]),
+      closed
+    );
+    return p;
+  }
+
+  static createFromSelection(s: PolygonalSelection) {
+    const p = new PolygonalSelection(s.points, s.closed);
+    p.setProperties(s.id, s.name, s.colour, s.alpha);
+    p.setFixed(s.fixed);
+    return p;
   }
 }
 
@@ -152,6 +250,41 @@ export class EllipticalSelection extends OrientableSelection {
     super(start, angle);
     this.semi_axes = semi_axes;
   }
+
+  static clicks() {
+    return [2, 3];
+  }
+
+  static isShape(
+    s: EllipticalSelection | SelectionBase
+  ): s is EllipticalSelection {
+    return 'semi_axes' in s;
+  }
+
+  static createFromPoints(points: Vector3[]) {
+    const c = points[0];
+    const i = new Vector3().subVectors(points[1], c);
+    if (points.length < 3) {
+      return new EllipticalSelection(
+        [c.x, c.y],
+        [Math.abs(i.x), Math.abs(i.y)]
+      );
+    }
+    const pi = polar(i);
+    const si = pi[0];
+    const o = new Vector3().subVectors(points[2], c);
+    const so = Math.hypot(o.x, o.y);
+    const ss = si < so ? [si, so] : [so, si];
+
+    return new EllipticalSelection([c.x, c.y], ss as [number, number], pi[1]);
+  }
+
+  static createFromSelection(s: EllipticalSelection) {
+    const e = new EllipticalSelection(s.start, s.semi_axes);
+    e.setProperties(s.id, s.name, s.colour, s.alpha);
+    e.setFixed(s.fixed);
+    return e;
+  }
 }
 
 /** export class to select a circle */
@@ -160,6 +293,26 @@ export class CircularSelection extends BaseSelection {
   constructor(start: [number, number], radius: number) {
     super(start);
     this.radius = radius;
+  }
+
+  static clicks() {
+    return [2, 2];
+  }
+
+  static isShape(s: CircularSelection | SelectionBase): s is CircularSelection {
+    return 'radius' in s;
+  }
+
+  static createFromPoints(points: Vector3[]) {
+    const [c, e] = points;
+    return new CircularSelection([c.x, c.y], Math.hypot(e.x, e.y));
+  }
+
+  static createFromSelection(s: CircularSelection) {
+    const c = new CircularSelection(s.start, s.radius);
+    c.setProperties(s.id, s.name, s.colour, s.alpha);
+    c.setFixed(s.fixed);
+    return c;
   }
 }
 
@@ -176,32 +329,125 @@ export class CircularSectorialSelection extends BaseSelection {
     this.radii = radii;
     this.angles = angles;
   }
+
+  static clicks() {
+    return [2, 3];
+  }
+
+  static isShape(
+    s: CircularSectorialSelection | SelectionBase
+  ): s is CircularSectorialSelection {
+    return 'radii' in s;
+  }
+
+  static createFromPoints(points: Vector3[]) {
+    const c = points[0];
+    const i = new Vector3().subVectors(points[1], c);
+    const pi = polar(i);
+    const ri = pi[0];
+    const ai = pi[1];
+    let ro = ri;
+    let ao = ai;
+    if (points.length > 2) {
+      const o = new Vector3().subVectors(points[2], c);
+      const po = polar(o);
+      ro = po[0];
+      ao = po[1];
+    }
+    const rs = ri < ro ? [ri, ro] : [ro, ri];
+    const as = ai < ao ? [ai, ao] : [ao, ai];
+
+    return new CircularSectorialSelection(
+      [c.x, c.y],
+      rs as [number, number],
+      as as [number, number]
+    );
+  }
+
+  static createFromSelection(s: CircularSectorialSelection) {
+    const cs = new CircularSectorialSelection(s.start, s.radii, s.angles);
+    cs.setProperties(s.id, s.name, s.colour, s.alpha);
+    cs.setFixed(s.fixed);
+    return cs;
+  }
+}
+
+function getSelectionType(selection: SelectionBase) {
+  if (RectangularSelection.isShape(selection)) {
+    return SelectionType.rectangle;
+  } else if (LinearSelection.isShape(selection)) {
+    return SelectionType.line;
+  } else if (PolygonalSelection.isShape(selection)) {
+    return selection.closed ? SelectionType.polygon : SelectionType.polyline;
+  } else if (EllipticalSelection.isShape(selection)) {
+    return SelectionType.ellipse;
+  } else if (CircularSelection.isShape(selection)) {
+    return SelectionType.circle;
+  } else if (CircularSectorialSelection.isShape(selection)) {
+    return SelectionType.sector;
+  } else {
+    return SelectionType.unknown;
+  }
+}
+
+export function recreateSelection(selection: SelectionBase) {
+  if (RectangularSelection.isShape(selection)) {
+    return RectangularSelection.createFromSelection(selection);
+  } else if (LinearSelection.isShape(selection)) {
+    return LinearSelection.createFromSelection(selection);
+  } else if (PolygonalSelection.isShape(selection)) {
+    return PolygonalSelection.createFromSelection(selection);
+  } else if (EllipticalSelection.isShape(selection)) {
+    return EllipticalSelection.createFromSelection(selection);
+  } else if (CircularSelection.isShape(selection)) {
+    return CircularSelection.createFromSelection(selection);
+  } else if (CircularSectorialSelection.isShape(selection)) {
+    return CircularSectorialSelection.createFromSelection(selection);
+  } else {
+    return null;
+  }
+}
+
+function createSelection(
+  clicks: boolean,
+  selectionType: SelectionType,
+  points: Vector3[]
+) {
+  switch (selectionType) {
+    case SelectionType.rectangle:
+      return RectangularSelection.createFromPoints(!clicks, points);
+    case SelectionType.sector:
+      return CircularSectorialSelection.createFromPoints(points);
+    case SelectionType.circle:
+      return CircularSelection.createFromPoints(points);
+    case SelectionType.ellipse:
+      return CircularSelection.createFromPoints(points);
+    case SelectionType.polygon:
+      return PolygonalSelection.createFromPoints(true, points);
+    case SelectionType.polyline:
+      return PolygonalSelection.createFromPoints(false, points);
+    case SelectionType.line:
+    case SelectionType.unknown:
+    default:
+      return LinearSelection.createFromPoints(points);
+  }
 }
 
 export function pointsToSelection(
-  selectionType: string,
+  selectionType: SelectionType,
   points: Vector3[],
   colour: string,
   alpha: number
 ): BaseSelection {
   console.debug('Points', selectionType, points);
-  let s: BaseSelection;
-  switch (selectionType) {
-    case 'rectangle':
-      s = RectangularSelection.createFromPoints(true, points);
-      break;
-    case 'line':
-    default:
-      s = LinearSelection.createFromPoints(points);
-      break;
-  }
+  const s = createSelection(false, selectionType, points);
   s.colour = colour;
   s.alpha = alpha;
   return s;
 }
 
-export function createShape(
-  selectionType: string,
+function createShape(
+  selectionType: SelectionType,
   points: Vector3[],
   colour: string,
   alpha: number
@@ -209,53 +455,51 @@ export function createShape(
   const props = {
     fill: colour,
     fillOpacity: alpha,
-    stoke: colour,
-    stokeWidth: 1,
+    stroke: colour,
+    strokeWidth: 1,
   };
   switch (selectionType) {
-    case 'rectangle':
+    case SelectionType.rectangle:
+    case SelectionType.polygon:
       return (
         <SvgElement>
           <DvdPolygon coords={points} {...props} />
         </SvgElement>
       );
-    case 'line':
-    default:
+    case SelectionType.line:
+    case SelectionType.polyline:
       return (
         <SvgElement>
           <DvdPolyline coords={points} {...props} />
         </SvgElement>
       );
+    case SelectionType.ellipse:
+    case SelectionType.circle:
+    case SelectionType.sector:
+    case SelectionType.unknown:
+    default:
+      return null;
   }
 }
 
 export function pointsToShape(
-  selectionType: string,
+  selectionType: SelectionType,
   points: Vector3[],
   colour: string,
   alpha: number
 ) {
-  let s: BaseSelection;
-  switch (selectionType) {
-    case 'rectangle':
-      s = RectangularSelection.createFromPoints(false, points);
-      break;
-    case 'line':
-    default:
-      s = LinearSelection.createFromPoints(points);
-  }
+  const s = createSelection(true, selectionType, points);
   return createShape(selectionType, s.getPoints(), colour, alpha);
 }
 
 function createSelectionShape(selection: SelectionBase) {
-  let selectionType: string;
-  if ('lengths' in selection) {
-    selectionType = 'rectangle';
-  } else {
-    selectionType = 'line';
-  }
-  if (selection.getPoints !== undefined) {
+  const selectionType = getSelectionType(selection);
+  if (
+    selectionType !== SelectionType.unknown &&
+    selection.getPoints !== undefined
+  ) {
     const pts = selection.getPoints();
+    console.debug('Shape points', selectionType, pts);
     return (
       <DataToHtml points={pts} key={selection.id}>
         {(...htmlSelection: Vector3[]) =>
@@ -269,6 +513,7 @@ function createSelectionShape(selection: SelectionBase) {
       </DataToHtml>
     );
   }
+  console.error('Unknown selection type or has no points getter', selection);
   return null;
 }
 
