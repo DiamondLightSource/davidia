cfc7c4fd6e4b028fa7023707c9479b6233bac93b
VictoriaBeilsten-Edmands <victoria.beilsten.edmands@gmail.com>
2023-05-17 14:16:57 +0100

    Add colour and icon to selection id dropdown

--- client/component/SelectionIDDropdown.tsx
+++ client/component/SelectionIDDropdown.tsx
@@ -1,4 +1,12 @@
+import Select, { StylesConfig } from 'react-select';
 import { getSelectionType } from './selections';
+import { ValueType } from 'react-select/lib/types';
+
+interface OptionType {
+  label: string;
+  value: string;
+  bgcolour: string;
+}
 
 interface SelectionIDDropdownProps {
   selections: SelectionBase[];
@@ -15,26 +23,69 @@ export function SelectionIDDropdown(props: SelectionIDDropdownProps) {
     options = props.selections.map((s) => s.id),
   } = props;
 
-  function getSelectionLabel(i: string) {
+  const selectionIcons = {
+    line: '\u2014',
+    rectangle: '\u25ad',
+    polyline: '\u299a',
+    polygon: '\u2b21',
+    circle: '\u25cb',
+    ellipse: '\u2b2d',
+    sector: '\u25d4',
+    unknown: ' ',
+  };
+
+  const defaultColour = '#ffffff';
+
+  function getSelectionLabel(i: string): string {
     const selection = props.selections.find((s) => s.id == i);
     if (selection != undefined) {
-      const colour = selection.colour ?? '';
-      const selectionType = getSelectionType(selection);
-      const selectionLabel = `${selection.name} ${colour} ${selectionType} ${i}`;
+      const selectionIcon = selectionIcons[getSelectionType(selection)];
+      const selectionLabel = `${selectionIcon} ${selection.name} ${i}`;
       return selectionLabel;
     } else {
-      return null;
+      return 'Choose selection';
     }
   }
 
+  function getSelectionColour(i: string) {
+    const selection = props.selections.find((s) => s.id == i);
+    return selection?.colour ?? defaultColour;
+  }
+
+  const selectStyles: StylesConfig<OptionType> = {
+    option: (provided, state) => {
+      return {
+        ...provided,
+        backgroundColor: state.data.bgcolour,
+      };
+    },
+  };
+
+  const optionComps = options.map((s) => ({
+    value: s,
+    label: getSelectionLabel(s),
+    bgcolour: getSelectionColour(s),
+  }));
+  optionComps.push({
+    value: '',
+    label: 'Choose selection',
+    bgcolour: defaultColour,
+  });
+
+  console.log('optionComps: ', optionComps);
+
   return (
-    <select value={value} onChange={(e) => onSelectionIDChange(e.target.value)}>
-      {options.map((s) => (
-        <option key={s} value={s}>
-          {getSelectionLabel(s)}
-        </option>
-      ))}
-      <option value={''}>Choose selection</option>
-    </select>
+    <Select
+      styles={selectStyles}
+      value={{
+        value: value,
+        label: getSelectionLabel(value),
+        bgcolour: getSelectionColour(value),
+      }}
+      options={optionComps}
+      onChange={(selectedOption: ValueType<OptionType>) =>
+        onSelectionIDChange((selectedOption as OptionType).value)
+      }
+    />
   );
 }
