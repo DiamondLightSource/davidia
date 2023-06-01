82b61c1c1001ddfe7501fcdd261073f608d75128
VictoriaBeilsten-Edmands <victoria.beilsten.edmands@gmail.com>
2023-06-01 10:38:46 +0100

    Make Modal draggable

--- client/component/Modal.tsx
+++ client/component/Modal.tsx
@@ -1,3 +1,4 @@
+import Draggable from 'react-draggable';
 import { ToggleBtn } from '@h5web/lib';
 import { useClickOutside, useKeyboardEvent } from '@react-hookz/web';
 import { useRef, useState } from 'react';
@@ -15,6 +16,10 @@ export interface ModalProps {
 export function Modal(props: ModalProps) {
   const rootRef = useRef(null);
   const [showModal, setShowModal] = useState(false);
+  const [defaultPosition, setDefaultPosition] = useState<{
+    x: number;
+    y: number;
+  }>({ x: 0, y: 0 });
 
   useClickOutside(rootRef, (e) => {
     e.stopPropagation(); // stop interactions outside modal
@@ -45,28 +50,38 @@ export function Modal(props: ModalProps) {
   );
 
   const modal = showModal ? (
-    <div hidden={!showModal} ref={rootRef} className={styles.modal}>
-      <div
-        className={styles.modal_content}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className={styles.modal_header}>
-          <h4 className={styles.modal_title}>
-            {props.title}
-            <button
-              onClick={() => {
-                setShowModal(false);
-              }}
-              className={styles.close_button}
-            >
-              X
-            </button>
-          </h4>
+    <Draggable
+      handle="strong"
+      defaultPosition={defaultPosition}
+      onStop={(e, data: { x: number; y: number }) => {
+        setDefaultPosition({ x: data.x, y: data.y });
+      }}
+    >
+      <div hidden={!showModal} ref={rootRef} className={styles.modal}>
+        <div
+          className={styles.modal_content}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <strong className="cursor">
+            <div className={styles.modal_header}>
+              <h4 className={styles.modal_title}>
+                {props.title}
+                <button
+                  onClick={() => {
+                    setShowModal(false);
+                  }}
+                  className={styles.close_button}
+                >
+                  X
+                </button>
+              </h4>
+            </div>
+          </strong>
+          <div className={styles.modal_body}> {props.children} </div>
+          <div className={styles.modal_footer}></div>
         </div>
-        <div className={styles.modal_body}> {props.children} </div>
-        <div className={styles.modal_footer}></div>
       </div>
-    </div>
+    </Draggable>
   ) : null;
 
   return [modal, toggle];
