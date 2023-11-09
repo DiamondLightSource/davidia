53d93803604fd1e5e38b33890ceec504e79bc496
Peter Chang <peter.chang@diamond.ac.uk>
2023-11-09 10:47:09 +0000

    Replace npm with pnpm (#112)
    Set build to use relative path so all client code is served through single mount

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   server: {
     port: 3000,
   },
