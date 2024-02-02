5f62a84b501ca9fb87587f1645c58e662ce68ea2
VictoriaBeilsten-Edmands <victoria.beilsten.edmands@gmail.com>
2024-02-02 14:44:38 +0000

    Add long description to setup.py & pnpm publish tag

--- davidia/setup.py
+++ davidia/setup.py
@@ -1,13 +1,30 @@
+import os
 from setuptools import find_packages, setup
 
 # from Davidia import __version__
 __version__ = "0.1.0"
 
 
+def find_readme():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(current_dir, "README.md")
+    if os.path.exists(path):
+        return path
+
+    raise FileNotFoundError(f"README.md not found in {current_dir}")
+
+
+readme_path = find_readme()
+
+
 setup(
     name="Davidia",
     version=__version__,
     description="Plot server with React frontend data visualiser",
+    long_description=open(readme_path).read(),
+    long_description_content_type="text/markdown",
+    package_data={"": [readme_path]} if readme_path else {},
+    include_package_data=True,
     author_email="dataanalysis@diamond.ac.uk",
     packages=find_packages(),
     python_requires=">=3.10",
@@ -23,7 +40,7 @@ setup(
         "uvicorn",
         "websockets",
     ],
-    extras_requires={
+    extras_require={
         "test": [
             "pytest",
             "pytest-asyncio",
